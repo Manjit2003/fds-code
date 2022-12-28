e4af1623db10a0ad0043018e1556504f32e8ea5c
Manjit Pardeshi <manjitpardeshi91991@gmail.com>
2022-12-28 20:15:07 +0530

    fix: remove setting permission from windows.bat

--- windows.bat
+++ windows.bat
@@ -1,6 +1,3 @@
-curl -L -o https://raw.githubusercontent.com/Manjit2003/fds-code/main/code.zip
+curl https://raw.githubusercontent.com/Manjit2003/fds-code/main/code.zip -o code.zip
 powershell -command "Expand-Archive -Path code.zip -DestinationPath C:\fds -Force"
 powershell -command "Remove-Item -Path code.zip -Force"
-powershell -command "Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope LocalMachine"
-powershell -command "Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser"
-powershell -command "Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process"
