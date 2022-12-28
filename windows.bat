0b1edc030eebfa164036c0af0e67457d78a26f05
Manjit Pardeshi <manjitpardeshi91991@gmail.com>
2022-12-28 20:22:24 +0530

    fix: change windows link to verce

--- windows.bat
+++ windows.bat
@@ -1,3 +1,3 @@
-curl https://raw.githubusercontent.com/Manjit2003/fds-code/main/code.zip -o code.zip
+curl https://fds-code.vercel.app/code.zip -o code.zip
 powershell -command "Expand-Archive -Path code.zip -DestinationPath C:\fds -Force"
 powershell -command "Remove-Item -Path code.zip -Force"
