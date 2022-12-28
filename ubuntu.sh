091386dc37b39e3fbac7af77c31de97a2529378b
Manjit Pardeshi <manjitpardeshi91991@gmail.com>
2022-12-28 20:25:28 +0530

    fix: change ubuntu url

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,4 +1,4 @@
 # Shell-script to download zip file from github and extract it 
-wget https://raw.githubusercontent.com/Manjit2003/fds-code/main/code.zip -O code.zip
+wget https://fds-code.vercel.app/code.zip -O code.zip
 unzip code.zip -d /home/code 
 rm code.zip
